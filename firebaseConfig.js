d0261e1a88aa27146528e95489cec9ea47804828
leonisescalunsag <calunsagleonises@gmail.com>
2025-09-16 11:24:34 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAr2WAVELb3O-XLnkGDaD7V5HinXTBercM",
+  authDomain: "qoutify-a4922.firebaseapp.com",
+  projectId: "qoutify-a4922",
+  storageBucket: "qoutify-a4922.firebasestorage.app",
+  messagingSenderId: "242003817221",
+  appId: "1:242003817221:web:f0baf6fb27da3cc21bfd6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
